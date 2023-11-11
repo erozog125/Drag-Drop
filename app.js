68cad3658d96ada5a79eb2e4dfa1378b8d211b2a
Edwin Rozo Gómez <erozog125@gmail.com>
2023-11-10 20:44:44 -0500

    Drag And drop

--- app.js
+++ app.js
@@ -5,5 +5,25 @@ const list = document.getElementById('list')
 Sortable.create(list, {
   animation: 100,
   swapThreshold: 1,
-  chosenClass: 'custom-list'
+  chosenClass: 'custom-list',
+  dragClass: 'drag',
+  // onEnd: función que se ejecuta cuando el usuario suelte
+  onEnd: () => console.log('funciona'),
+  group:'task-list',
+  // guardar en localStorage
+  store: {
+    // Guardar los elementos de mi lista de tareas
+    set: sortable => {
+      const order = sortable.toArray()
+      // console.log(order)
+      // localStorage guarda strings
+      localStorage.setItem(sortable.options.group.name,order.join(','))
+    },
+    // Actualizar siempre el orden de nuestra lista de tareas
+    get: sortable => {
+      const order = localStorage.getItem(sortable.options.group.name)
+      console.log(order)
+      return order ? order.split(','): []
+    }
+  }
 })
